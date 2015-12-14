b9ab51f40b35e8cf538d85f33bb3b6fd64c5846b
AlvaradoAdam15 <adamalvarado@iesebre.com>
2015-12-14 16:38:35 +0100

    Change name app MenuWithAuthentication and create menu.php

---
+++ app/bootsrap/menu.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adam
+ * Date: 14/12/15
+ * Time: 16:29
+ */
+
+//name/title
+//url
+//Icona
+//Té submenus
+
+//Home
+MenuWithAuthentication::menu
+//Another link
+MenuWithAuthentication::menu
+
+//Multilevel
+MenuWithAuthentication::menu
+    MenuWithAuthentication::menu
+    MenuWithAuthentication::menu
