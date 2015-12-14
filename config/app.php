0cb9f33d3a3d85dc24626d3af838bcb940c8d6ca
AlvaradoAdam15 <adamalvarado@iesebre.com>
2015-12-14 19:54:54 +0100

    Add MenuWithAuthentication

--- config/app.php
+++ config/app.php
@@ -198,6 +198,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
         'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
+        'MenuWithAuthentication' => MenuWithAuthentication\MenuWithAuthentication::class,
 
     ],
 
